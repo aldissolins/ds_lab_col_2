8858ea050fc3d26ac4efbf2e7b714f899300d029
Aldis Šolins <79791153+aldissolins@users.noreply.github.com>
2024-05-20 07:48:31 +0000

    20.05.2024.

--- heap.py
+++ heap.py
@@ -16,55 +16,56 @@ class MaxHeap:
 
     def insert(self, value):
         self.heap.append(value)
-        current = len(self.heap) - 1
+        self._heapify_up(len(self.heap) - 1)
 
-        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
-            self._swap(current, self._parent(current))
-            current = self._parent(current)
+    def _heapify_up(self, index):
+        while index > 0 and self.heap[index] > self.heap[self._parent(index)]:
+            self._swap(index, self._parent(index))
+            index = self._parent(index)
 
-    def _sink_down(self, index):
-        max_index = index
+    def _heapify_down(self, index):
         while True:
+            max_index = index
             left_index = self._left_child(index)
             right_index = self._right_child(index)
 
-            if (left_index < len(self.heap) and 
-                    self.heap[left_index] > self.heap[max_index]):
+            if left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]:
                 max_index = left_index
 
-            if (right_index < len(self.heap) and 
-                    self.heap[right_index] > self.heap[max_index]):
+            if right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]:
                 max_index = right_index
 
             if max_index != index:
                 self._swap(index, max_index)
                 index = max_index
             else:
-                return
-                       
+                break
+
     def remove(self):
         if len(self.heap) == 0:
             return None
-
         if len(self.heap) == 1:
             return self.heap.pop()
-
+        
         max_value = self.heap[0]
         self.heap[0] = self.heap.pop()
-        self._sink_down(0)
-
+        self._heapify_down(0)
         return max_value
 
+    def peek(self):
+        if len(self.heap) == 0:
+            return None
+        return self.heap[0]
 
 def find_kth_smallest(nums, k):
-    ###################################
-    ###                             ###
-    ###                             ###
-    ###                             ###
-    ###################################
+    max_heap = MaxHeap()
+    
+    for num in nums:
+        max_heap.insert(num)
+        if len(max_heap.heap) > k:
+            max_heap.remove()
 
-
-
+    return max_heap.peek()
 
 # Test cases
 nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
@@ -80,34 +81,4 @@ for i in range(len(nums)):
     print(f'Test passed: {result == expected_outputs[i]}')
     print('---------------------------------------')
 
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Test case 1...
-    Input: [3, 2, 1, 5, 6, 4] with k = 2
-    Output: 2
-    Expected output: 2
-    Test passed: True
-    ---------------------------------------
-    Test case 2...
-    Input: [6, 5, 4, 3, 2, 1] with k = 3
-    Output: 3
-    Expected output: 3
-    Test passed: True
-    ---------------------------------------
-    Test case 3...
-    Input: [1, 2, 3, 4, 5, 6] with k = 4
-    Output: 4
-    Expected output: 4
-    Test passed: True
-    ---------------------------------------
-    Test case 4...
-    Input: [3, 2, 3, 1, 2, 4, 5, 5, 6] with k = 7
-    Output: 5
-    Expected output: 5
-    Test passed: True
-    ---------------------------------------
-
-"""
 
